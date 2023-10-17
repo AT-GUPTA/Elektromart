e732054900994b79c937f0cb868823046b6c21a9
ruksmita <ruksmita@gmail.com>
2023-10-17 19:55:37 -0400

    Improve style and layout

--- source/ui/elektromart/src/pages/products/products.js
+++ source/ui/elektromart/src/pages/products/products.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link } from 'react-router-dom'; // Import Link from React Router
+import { Link } from 'react-router-dom';
+import "../../styles/products.css";
 
 const products = require('./products.json');
 
@@ -12,13 +13,18 @@ function Products() {
         {products.map((product) => (
           <div className="col-md-4 mb-4" key={product.id}>
             <div className="card">
+              <img
+                src={product.image} // Add the product image URL here
+                alt={product.name}
+                className="card-img-top"
+              />
               <div className="card-body">
                 <h5 className="card-title">{product.name}</h5>
                 <p className="card-text">{product.description}</p>
                 <p className="card-text">Vendor: {product.vendor}</p>
                 <div className="d-flex justify-content-between align-items-center">
                   <p>Price: ${product.price.toFixed(2)}</p>
-                  <Link to={`/products/${product.id}`} className="btn btn-primary">
+                  <Link to={`/products/${product.id}`} className="btn btn-primary productsButton">
                     Buy
                   </Link>
                 </div>
