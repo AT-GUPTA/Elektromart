80aa39b958610cdcf089c345d24a2b562d02f84d
Atul Gupta <atulg@12221@gmail.com>
2023-10-24 18:14:20 -0400

    Linked cafrt content to api

--- source/api/Elektromart/src/main/java/com/Elektromart/Controller/CartController.java
+++ source/api/Elektromart/src/main/java/com/Elektromart/Controller/CartController.java
@@ -3,6 +3,9 @@ package com.elektromart.controller;
 import com.elektromart.domain.Cart;
 import com.elektromart.service.CartService;
 
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -21,6 +24,14 @@ public class CartController extends HttpServlet {
         if ("/getCart".equals(path)) {
             String cartId = req.getParameter("cartId");
             Cart cart = cartService.getCartById(cartId);
+            resp.setContentType("application/json");
+            JsonArray cartContent = cartService.getCartContent(cartId);
+            JsonObject jsonResponse = Json.createObjectBuilder()
+                    .add("status", "SUCCESS")
+                    .add("message", "Cart fetched successfully.")
+                    .add("cartContent", cartContent)
+                    .build();
+            resp.getWriter().write(jsonResponse.toString());
             // todo Convert cart object to JSON and send as response.
         }
         //todo handle other routes for changeItemQuantity and delete-from-cart
--- source/api/Elektromart/src/main/java/com/Elektromart/service/CartService.java
+++ source/api/Elektromart/src/main/java/com/Elektromart/service/CartService.java
@@ -3,12 +3,43 @@ package com.elektromart.service;
 import com.elektromart.dao.CartDao;
 import com.elektromart.domain.Cart;
 
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+
 public class CartService {
 
     private final CartDao cartDao = new CartDao();
 
     public Cart getCartById(String cartId) {
         return cartDao.getCartById(cartId);
+    }
+
+    public JsonArray getCartContent(String cartId) {
+        JsonObject iphone = Json.createObjectBuilder()
+                .add("id", 1)
+                .add("name", "iPhone 13 Pro")
+                .add("description", "The iPhone 13 Pro is Apple's latest flagship smartphone, featuring a stunning Super Retina XDR display, A15 Bionic chip, and advanced camera system.")
+                .add("price", 999.99)
+                .add("quantity", 1)
+                .add("imageUrl", "/images/iphone.jpg")
+                .build();
+
+        // Samsung Galaxy S21 data
+        JsonObject samsung = Json.createObjectBuilder()
+                .add("id", 2)
+                .add("name", "Samsung Galaxy S21")
+                .add("description", "The Samsung Galaxy S21 offers a sleek design, high-quality display, and a powerful processor for a smooth user experience.")
+                .add("price", 799.99)
+                .add("quantity", 2)
+                .add("imageUrl", "/images/galaxy.jpg")
+                .build();
+
+        // Create JsonArray and add the above objects
+        return Json.createArrayBuilder()
+                .add(iphone)
+                .add(samsung)
+                .build();
     }
 
     // todo Add methods for changeItemQuantity and delete-from-cart
