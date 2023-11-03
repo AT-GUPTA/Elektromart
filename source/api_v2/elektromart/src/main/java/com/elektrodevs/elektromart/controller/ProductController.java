7ad389fe64458091f0352bc891921b8be6346c8e
myriamshawi <shawi.myriam@gmail.com>
2023-11-03 15:56:16 -0400

    adjusment to api/ui product

--- source/api_v2/elektromart/src/main/java/com/elektrodevs/elektromart/controller/ProductController.java
+++ source/api_v2/elektromart/src/main/java/com/elektrodevs/elektromart/controller/ProductController.java
@@ -4,40 +4,65 @@ import com.elektrodevs.elektromart.domain.Product;
 import com.elektrodevs.elektromart.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 @RequiredArgsConstructor
-@CrossOrigin
+@CrossOrigin (origins = "http://localhost:3000")
 public class ProductController {
 
     private final ProductService productService;
 
     @GetMapping("/")
     public ResponseEntity<List<Product>> getAllProducts() {
-        return ResponseEntity.ok(productService.getProducts());
+        List<Product> products = productService.getProducts();
+        return  ResponseEntity.ok(products);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Product> getProductById(@PathVariable int id) {
         Product product = productService.getProductById(id);
-        return product != null ? ResponseEntity.ok(product) : ResponseEntity.notFound().build();
+        if (product != null) {
+            return ResponseEntity.ok(product);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @GetMapping("/slug/{urlSlug}")
+    public ResponseEntity<Product> getProductById(@PathVariable String urlSlug) {
+        List<Product> products = productService.getProducts();
+        Product product = products.stream().filter(e -> e.getUrlSlug().equalsIgnoreCase(urlSlug)).toList().get(0);
+
+        if (product != null) {
+            return ResponseEntity.ok(product);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PostMapping("/")
-    public ResponseEntity<Product> createProduct(@RequestBody Product newProduct) {
+    public ResponseEntity<?> createProduct(@RequestBody Product newProduct) {
         Product product = productService.createProduct(newProduct);
-        return ResponseEntity.ok(product);
+        return (product != null)
+                ? ResponseEntity.ok(product)
+                : ResponseEntity.badRequest().body("Error");
     }
 
+
     @PutMapping("/{id}")
-    public ResponseEntity<Product> updateProduct(@PathVariable int id, @RequestBody Product updatedProduct) {
+    public ResponseEntity<?> updateProduct(@PathVariable int id, @RequestBody Product updatedProduct) {
         updatedProduct.setId(id);
         Product product = productService.editProduct(updatedProduct);
-        return ResponseEntity.ok(product);
+        return (product != null)
+                ? ResponseEntity.ok(product)
+                : ResponseEntity.badRequest().body("Error can't update");
     }
+
 }
