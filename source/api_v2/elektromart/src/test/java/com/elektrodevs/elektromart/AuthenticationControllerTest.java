fc75a889a3e8b6f66bc4dadcb19e85a3ab6a76b7
myriamshawi <shawi.myriam@gmail.com>
2023-12-02 12:15:29 -0500

    merging changes from main-v4

--- source/api_v2/elektromart/src/test/java/com/elektrodevs/elektromart/AuthenticationControllerTest.java
+++ source/api_v2/elektromart/src/test/java/com/elektrodevs/elektromart/AuthenticationControllerTest.java
@@ -5,23 +5,18 @@ import com.elektrodevs.elektromart.dto.JwtAuthenticationResponse;
 import com.elektrodevs.elektromart.dto.SignInRequest;
 import com.elektrodevs.elektromart.dto.SignUpRequest;
 import com.elektrodevs.elektromart.service.AuthenticationService;
-import com.elektrodevs.elektromart.service.JwtService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 /**
- * Test class for login and sigunp with passcode
+ * Test class for login and sigunp and change passcode
  */
 @ExtendWith(MockitoExtension.class)
 public class AuthenticationControllerTest {
@@ -29,43 +24,8 @@ public class AuthenticationControllerTest {
     @Mock
     private AuthenticationService authenticationService;
 
-    @Mock
-    private JwtService jwtService;
-
     @InjectMocks
     private AuthenticationController authenticationController;
-
-
-    /**
-     * Tests the login functionality to ensure proper handling of valid login requests.
-     */
-    @Test
-    public void testLogin() {
-        // Arrange
-        SignInRequest signInRequest = new SignInRequest();
-        signInRequest.setUsername("testUser");
-        signInRequest.setPassword("testPass");
-
-        UserDetails mockUserDetails = new User("testUser", "password123", Collections.emptyList());
-        String dummyToken = "dummyToken";
-        JwtAuthenticationResponse jwtResponse = new JwtAuthenticationResponse();
-        jwtResponse.setToken(dummyToken);
-
-        when(jwtService.generateToken(any(UserDetails.class))).thenReturn(dummyToken);
-        when(authenticationService.login(any(SignInRequest.class))).thenReturn(jwtResponse);
-
-        // Act
-        ResponseEntity<?> response = authenticationController.login(signInRequest);
-
-        // Assert
-        assertEquals(200, response.getStatusCodeValue(), "Expected HTTP 200 OK response");
-        assertNotNull(response.getBody(), "Response body should not be null");
-
-        JwtAuthenticationResponse responseBody = (JwtAuthenticationResponse) response.getBody();
-        assertEquals(dummyToken, responseBody.getToken(), "Expected token to match dummy token");
-
-        verify(authenticationService).login(any(SignInRequest.class));
-    }
 
 
     /**
@@ -76,7 +36,6 @@ public class AuthenticationControllerTest {
         // Arrange
         SignUpRequest signUpRequest = new SignUpRequest();
         signUpRequest.setEmail("test@example.com");
-        signUpRequest.setPassword("password123");
         signUpRequest.setUsername("testUser");
 
         JwtAuthenticationResponse jwtResponse = new JwtAuthenticationResponse();
@@ -90,4 +49,37 @@ public class AuthenticationControllerTest {
         assertEquals(200, response.getStatusCodeValue());
         verify(authenticationService).signup(any(SignUpRequest.class));
     }
+
+
+    /**
+     * Tests the login functionality to ensure proper handling of valid login requests.
+     */
+    @Test
+    public void testLogin() {
+        // Arrange
+        SignInRequest signInRequest = new SignInRequest();
+        signInRequest.setUsername("testUser");
+        // No password is set here
+
+        JwtAuthenticationResponse jwtResponse = new JwtAuthenticationResponse();
+        jwtResponse.setToken("dummyToken");
+
+        when(authenticationService.login(any(SignInRequest.class))).thenReturn(jwtResponse);
+
+        // Act
+        ResponseEntity<?> response = authenticationController.login(signInRequest);
+
+        // Assert
+        assertEquals(200, response.getStatusCodeValue(), "Expected HTTP 200 OK response");
+        assertNotNull(response.getBody(), "Response body should not be null");
+
+        JwtAuthenticationResponse responseBody = (JwtAuthenticationResponse) response.getBody();
+        assertEquals("dummyToken", responseBody.getToken(), "Expected token to match dummy token");
+
+        verify(authenticationService).login(any(SignInRequest.class));
+    }
+
+    /**
+     * Tests change passcode functionality to ensure proper handling.
+     */
 }
